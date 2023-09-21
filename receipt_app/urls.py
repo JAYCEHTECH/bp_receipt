16a4222020bd7f2a9035a753bca8093c32113be4
mikegyamfi <codeage20@gmail.com>
2023-09-21 18:43:15 +0000

    initial commit

--- receipt_app/urls.py
+++ receipt_app/urls.py
@@ -1,10 +1,12 @@
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
+from django.conf import settings as conf_settings
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('generate/<str:invoice_number>/<str:channel>/<str:account_no>/<str:account_name>/<str:description>/<str:amount>', views.receipt_generator, name='receipt_generator'),
     path('<str:url_id>', views.url_redirect, name='url_redirect'),
     path('receipt/<str:invoice_number>/<str:channel>/<str:account_no>/<str:account_name>/<str:description>/<str:amount>', views.show_receipt, name='show_receipt')
-]
+] + static(conf_settings.STATIC_URL, document_root=conf_settings.STATIC_ROOT)
 
