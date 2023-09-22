ce078a698d854944475597c26efebb6842f33116
mikegyamfi <codeage20@gmail.com>
2023-09-22 06:41:12 +0000

    initial commit

--- receipt_app/admin.py
+++ receipt_app/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 from receipt_app import models
 
+
+class UrlDataAdmin(admin.ModelAdmin):
+    list_display = ['invoice_number', 'url_id', 'short_url']
+
+
 # Register your models here.
-admin.site.register(models.UrlData)
+admin.site.register(models.UrlData, UrlDataAdmin)
 
 
 
