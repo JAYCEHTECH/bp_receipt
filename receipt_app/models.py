8569ce1d7ca96fde1df953f61031259541d4f408
mikegyamfi <codeage20@gmail.com>
2023-09-22 06:59:37 +0000

    initial commit

--- receipt_app/models.py
+++ receipt_app/models.py
@@ -2,17 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# # Create your models here.
-# # Create your models here.
-# class CustomUser(AbstractUser):
-#     first_name = models.CharField(max_length=100, null=False, blank=False)
-#     last_name = models.CharField(max_length=100, null=False, blank=False)
-#     username = models.CharField(max_length=100, null=False, blank=False, unique=True)
-#     email = models.EmailField(max_length=250, null=False, blank=False)
-#     wallet = models.FloatField(null=True, blank=True, default=0.0)
-#     phone = models.PositiveIntegerField(null=True, blank=True)
-#     password1 = models.CharField(max_length=100, null=False, blank=False)
-#     password2 = models.CharField(max_length=100, null=False, blank=False)
+# Create your models here.
+# Create your models here.
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    email = models.EmailField(max_length=250, null=False, blank=False)
+    password1 = models.CharField(max_length=100, null=False, blank=False)
+    password2 = models.CharField(max_length=100, null=False, blank=False)
 
 
 class UrlData(models.Model):
